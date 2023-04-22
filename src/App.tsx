907a5030d380075706539a3059ec1c6a587b8187
Sharvil Patel <sharvilp018@gmail.com>
2023-04-22 15:43:54 +0530

    functionality done

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import SideBarComponent from "./components/SideBarComponent/SideBarComponent";
+import Contact from "./components/Contact/Contact";
+import ChartAndMaps from "./components/ChartAndMaps/ChartAndMaps";
+import AddContact from "./components/Contact/AddContact";
+import EditContact from "./components/Contact/EditContact";
+import {QueryClientProvider,QueryClient} from "react-query"
+
+
+const queryClient = new QueryClient()
+const App = () => {
+  return (
+ <>
+     <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+       <SideBarComponent>
+         <Routes>
+           {/* <Route path="/" element={<Contact />} /> */}
+           <Route path="/contact" element={<Contact />} />
+           <Route  path="/contact/add" element={<AddContact />} />
+           <Route path="/contact/edit/:id" element={ <EditContact />} />
+           <Route path="/" element={<ChartAndMaps />} />
+         </Routes>
+       </SideBarComponent>
+       </QueryClientProvider>
+     </BrowserRouter>
+   
+
+  
+    </>
+  );
+};
+
+export default App;
